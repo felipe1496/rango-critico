11f591234840bc3361c03c888fd4d49140494151
felipe1496 <felipe.oliveira1496@gmail.com>
2025-07-19 02:15:47 -0300

    fix eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,7 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  { "@typescript-eslint/no-empty-object-type": "0" },
 ];
 
 export default eslintConfig;
